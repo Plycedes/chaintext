e801f84abf3ccc2507c1887cf98cb119ae02122c
Plycedes <abhaypratapyadav340@gmail.com>
2023-12-25 00:58:13 +0530

    Images

---
+++ Assets/index.js
@@ -0,0 +1,27 @@
+import logo from "./logo.png";
+import close from "./cross.png";
+import open from "./menu.png";
+import search from "./zoom.png";
+import smile from "./smile.png";
+import send from "./send.png";
+import file from "./file.png";
+import user from "./user.png";
+import clear from "./delete.png";
+import create from "./add.png";
+import dot from "./record.png";
+import loader from "./loader.gif";
+
+export default {
+    logo,
+    close,
+    open,
+    search,
+    smile,
+    send,
+    file,
+    user,
+    clear,
+    create,
+    dot,
+    loader,
+};
--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,4 +1,10 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import Style from "./NavBar.module.css";
+import { Model, Error } from "../index";
+import { ChatAppContext } from "@/context/ChatAppContext";
 
 const NavBar = () => {
     return <div>NavBar</div>;
